94daf40ee5d5f56afdf68d522ee60e94599757e5
tinchogodoy <36281666+tinchogodoy@users.noreply.github.com>
2018-02-08 22:13:58 -0300

    Ejercicios 3, 4, 5 y el 6 falta

---
+++ jsEntradaSalida-5.js
@@ -0,0 +1,16 @@
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function Mostar()
+{	
+	
+	var nombre;
+	var edad;
+
+	//nombre=prompt("ingresa tu nombre");
+	nombre=document.getElementById('elNombre').value;
+	edad=document.getElementById('laEdad').value;
+	alert("Usted se llama "+nombre+" y tienes "+edad+" años");
+
+	
+}
+
---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,20 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+Mostar el resulto por medio de "ALERT"*/
+function sumar()
+{
+
+	var numeroUno;
+	var numeroDos;
+
+	//nombre=prompt("ingresa tu nombre");
+	numeroUno=document.getElementById('elNombre').value;
+	numeroDos=document.getElementById('laEdad').value;
+	alert("Usted se llama "+nombre+" y tienes "+edad+" años");
+
+	
+
+
+
+}
+
